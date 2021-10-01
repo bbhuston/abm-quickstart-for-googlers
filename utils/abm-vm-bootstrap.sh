3511b0ca3e6ca39c8e2eca044af0301fa25e0f4d
Ben Huston <bbhuston@gmail.com>
2021-10-01 09:42:25 -0400

    Stub out kubevirt vm upload and asm install Makefile targets

--- utils/abm-vm-bootstrap.sh
+++ utils/abm-vm-bootstrap.sh
@@ -99,6 +99,23 @@ cd ~
 echo "Installing docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+
+echo "Installing Krew"
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"${OS}_${ARCH}" &&
+  "$KREW" install krew
+)
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+kubectl krew
+
+echo "Installing Virt plugin"
+kubectl krew install virt
 EOF
 
 # Register the ABM workstation's SSH public key with each VM
